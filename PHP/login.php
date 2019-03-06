71cf30d88aad5c8d5912225f18dabf996d41593b
jackTGarland <jacktgarland@gmail.com>
2019-03-06 19:19:24 +0000

    Added login.php Used to connect to the mySQL server and ask for details.

---
+++ PHP/login.php
@@ -0,0 +1,18 @@
+<?PHP
+$authFile = fopen("auth.txt", "r");
+$username = fgets($authFile);
+$password = fgets($authFile);
+fclose($authFile);
+$a = $_GET["course"];
+$conn = new PDO("mysql:host=localhost;dbname=student_records;", $username, $password);
+
+$results = $conn>query("SELECT * FROM student WHERE course='$a'");
+while($row=$results->fetch())
+{
+    echo "<P>";
+    echo " student ID ". $row["ID"] . "<br/>";
+    echo " name " . $row["name"] . "<br/>";
+    echo "phone " . $row["phone"] . "<br/>" ;
+    echo "</P>";
+}
+?>
