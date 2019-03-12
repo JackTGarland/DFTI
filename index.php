48bb88d2e146ff61f2b6fe6019def50fa15c11f9
JackTGarland <jacktgarland@gmail.com>
2019-03-12 23:10:34 +0000

    made updates to the main php
    added more code to index.php that will be used for devlopment of main site.

--- index.php
+++ index.php
@@ -1,7 +1,37 @@
 <?php
 session_start();
-includes 'login.php';
-includes 'logout.php';
+#includes 'PHP/login.php';
+#includes 'PHP/logout.php';
+?>
+<!doctype html>
+<html>
+
+<head>
+<title>test</title>
+<script type ='text/javascript'>
+function status()
+{
+    var xdata = <?php echo json_encode($_SESSION["username"]); ?>;
+    if (xdata == null){
+        document.getElementById("status").innerHTML = "You are not logged in.";
+    }else{
+        document.getElementById('status').innerHTML = "You are logged in.";
+    };
+
+}
+</script>
+</head>
+
+<body onload="status()">
+<p id="status"></p>
+<input id="username" /><br/>
+<input id="password" /><br/>
+<input type="button" value="login"  />
+
+</body>
+<?php
+
 echo "hello world";
 echo "This is just a test";
 ?>
+</html>
